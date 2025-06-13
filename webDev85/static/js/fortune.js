85daba1b281188fcfa84e169f48f4f1793f58780
Impana <impana05@gmail.com>
2025-06-14 00:25:22 +0530

    link

--- webDev85/static/js/fortune.js
+++ webDev85/static/js/fortune.js
@@ -17,7 +17,7 @@ async function breakCookie(position) {
 
     try {
       // Fetch fortune from Flask LLM endpoint
-      const response = await fetch('http://localhost:5000/getFortuneLLM');
+      const response = await fetch('https://randomverse.onrender.com/getFortuneLLM');
       if (!response.ok) throw new Error("Network response was not ok");
       const data = await response.json();
 
